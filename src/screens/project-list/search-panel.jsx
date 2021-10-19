8bf1bf4b277b8bb2d806e3378a53ab7f0240bcc9
zhengdai <798484315@qq.com>
2021-10-19 16:44:55 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,21 @@
+export  const SearchPanel = ({param, setParam, users}) => {
+
+
+  return <form action="">
+    <div>
+      <input type="text" value={param.name} onChange={evt => setParam({
+        ...param,
+        name: evt.target.value
+      })}/>
+      <select value={param.personId} onChange={evt => setParam({
+        ...param,
+        personId: evt.target.value
+      })}>
+        <option value={''}>负责人</option>
+        {
+          users.map(user => <option value={user.id}>{user.name}</option>)
+        }
+      </select>
+    </div>
+  </form>
+}
