f1b6d00f1a35155fe59456f7a98af075c188eaac
zhengdai <798484315@qq.com>
2021-10-20 19:19:22 +0800

    fix: 实现useArray

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -31,3 +31,18 @@ export const useDebounce = <V>(value: V, delay?: number) => {
   }, [value, delay]);
   return debounceValue;
 };
+
+export const useArray = <T>(initialArray: T[])  => {
+  const [value, setValue] = useState(initialArray);
+  return {
+    value,
+    setValue,
+    add:(item: T) => setValue([...value, item]),
+    clear: () => setValue([]),
+    removeIndex(index:number) {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    }
+  }
+};
