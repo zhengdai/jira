ca18f634c85faab9c3d6c6d6d154258c1d45aeab
zhengdai <798484315@qq.com>
2021-10-29 08:07:00 +0800

    style: 优化报错

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -3,7 +3,7 @@ import {ProjectListScreen} from "./screens/project-list";
 import styled from "@emotion/styled";
 import { Row } from "components/lib";
 import {ReactComponent as SoftwareLogo} from "assets/software-logo.svg";
-import {Dropdown, Menu} from "antd";
+import {Button, Dropdown, Menu} from "antd";
 
 export const AuthenticatedApp = () => {
     const {logout, user } = useAuth();
@@ -19,11 +19,11 @@ export const AuthenticatedApp = () => {
                     <Dropdown overlay={
                         <Menu>
                             <Menu.Item key={"logout"}>
-                                <a onClick={logout}>登出</a>
+                                <Button type={"link"} onClick={logout}>登出</Button>
                             </Menu.Item>
                         </Menu>
                     }>
-                        <a onClick={e => e.preventDefault()}>Hi, {user?.name}</a>
+                        <Button type={"link"} onClick={e => e.preventDefault()}>Hi, {user?.name}</Button>
                     </Dropdown>
                 </HeaderRight>
             </Header>
